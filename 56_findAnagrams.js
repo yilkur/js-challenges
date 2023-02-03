f2edeae760512ed7ababda695caf0ed83c869a03
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2023-02-03 23:55:09 +0100

    solve challenge 56

---
+++ 56_findAnagrams.js
@@ -0,0 +1,52 @@
+/* Find anagrams in an array   
+
+When two words have the exact same letters, they are anagrams. 
+
+Each item in the anagrams array is an anagram of a Scrimba teacher's
+first and last name, plus the phrase "Scrimba teacher". 
+
+Write a function to determine which strings in the array are 
+anagrams of "Bob Ziroll Scrimba Teacher".
+
+Your function should take two parameters: the phrase you want to compare to
+the anagrams, and an array of anagrams. The function should return
+a new array of anagrams that match the phrase. 
+
+Example input: treat, ["tater", "tree", "teart", "tetra", "heart", "hamster"]
+Example output: ["tater", "teart", "tetra"]
+
+Bonus: What other teachers are represented in these anagrams? 
+ */
+
+const anagrams = [
+  'moz biblical torchbearers',
+  "it's razorbill beachcomber",
+  'och mcrobbie trailblazers',
+  'bib chorizo cellarmaster',
+  'thor scribble carbimazole',
+  'zilla borscht abercrombie',
+  'brazil scorcher batmobile',
+  'dame shelburne characterizing',
+  'uber englishman characterized',
+  'agnes rhumbline characterized',
+  'rehab scrutinized charlemagne',
+  'dreams zurich interchangeable',
+  'bam hamster technocratic',
+  'mechatronic masterbatch',
+  'bam ratchet mechatronics',
+]
+
+const sortPhrase = phrase =>
+  phrase.toLowerCase().split('').sort().join('').trim()
+
+const isAnagramInArray = (anagram, arr) => {
+  return arr.filter(item => {
+    const word1 = sortPhrase(anagram)
+    const word2 = sortPhrase(item)
+
+    return word1 === word2
+  })
+}
+
+console.log(isAnagramInArray('Tom Chant Scrimba Teacher', anagrams))
+console.log(isAnagramInArray('Bob Ziroll Scrimba Teacher', anagrams))
